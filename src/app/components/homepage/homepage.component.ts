2b4f2aecba373aef80ff50dd9e3f9b4254ac2537
coniarojoannamae <coniarojoannamae093@gmail.com>
2024-09-20 01:32:24 +0800

    First Commit

---
+++ src/app/components/homepage/homepage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  standalone: true,
+  styleUrl: './homepage.component.css'
+})
+export class HomepageComponent {
+
+}
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  standalone: true,
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
